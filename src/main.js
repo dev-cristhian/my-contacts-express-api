89ce4a6f23b0504db9fe85bab4b6601e0ff42603
Cristhian Santos <cristhianfelipe0@gmail.com>
2025-07-28 13:10:25 -0300

    ✨ feat: add middleware for JSON parsing, CORS handling, and 404 response in main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,17 @@ import { router } from "./route.js";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+app.use((_, response, next) => { // Middleware to handle CORS
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(router);
+
+app.use((_, response) => (response.status(404).send({ error: "Not Found" })));
+
 app.listen(port, () => console.log(`🚀 server is running on http://localhost:${port}`));
