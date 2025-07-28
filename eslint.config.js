aa2cf3c4830aff305e33f15165687c5e8954280e
Cristhian Santos <cristhianfelipe0@gmail.com>
2025-07-27 21:19:12 -0300

    💄 style: adds prettier and eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      eqeqeq: ["error", "always"],
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-alert": "warn",
+      "no-console": "warn",
+      "no-unused-vars": ["error", { args: "none" }],
+      "no-redeclare": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "object-shorthand": ["error", "always"],
+      "no-duplicate-imports": "error",
+      "prefer-template": "warn",
+    },
+  },
+]);
